03d82639b8459539764ba7f9e47446b17133f130
gjones81 <jones.guy81@gmail.com>
2020-03-29 13:31:02 -0700

    Set HTML structure

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('JS is working')
+
+const lighten = (e) => {
+	document.getElementById('test').style.visibility = 'hidden'
+}
+
+const darken = (e) => {
+	document.getElementById('test').style.visibility = 'visible'
+}
+
+document.getElementById('test').setEventListener('mouseenter', lighten)
+document.getElementById('test').setEventListener('mouseleave', darken)
