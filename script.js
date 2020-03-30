1908cc0195efe1f3382f83813736ec9a4a089094
gjones81 <jones.guy81@gmail.com>
2020-03-30 14:48:48 -0700

    working on HTML/CSS

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let startButton = document.getElementById('startButton')
 
 
+const lighten = (e) => {
+	e.target.opacity = '0';
+	console.log(e.target)
+}
+
+const darken = (e) => {
+	e.target.opacity = '1'
+}
+
+document.getElementById('15').addEventListener('mouseenter', lighten)
+document.getElementById('15').addEventListener('mouseleave', darken)
+
+
+
 //Start button event listener calls setGame
-startButton.addEventListener('click', setGame) 
+//startButton.addEventListener('click', setGame)
