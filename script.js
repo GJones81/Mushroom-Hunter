74d88ac9189e0f08bc0659f00b2afafa594593a5
gjones81 <jones.guy81@gmail.com>
2020-03-30 15:45:57 -0700

    Mouse events don't work with different z indexes. Changing direction

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let startButton = document.getElementById('startButton')
 
 
-const lighten = (e) => {
-	e.target.opacity = '0';
-	console.log(e.target)
-}
+// const lighten = (e) => {
+// 	e.target.style.backgroundColor = 'yellow'
+// 	console.log(e.target)
+// }
 
-const darken = (e) => {
-	e.target.opacity = '1'
-}
+// const darken = (e) => {
+// 	e.target.style.backgroundColor = 'black'
+// 	console.log(e.target)
+// }
 
-document.getElementById('15').addEventListener('mouseenter', lighten)
-document.getElementById('15').addEventListener('mouseleave', darken)
+// const ping = (e) => {
+// 	e.target.style.backgroundColor = 'yellow'
+// 	console.log(e.target)
+// }
 
+// document.getElementById('15').addEventListener('mouseenter', lighten)
+// document.getElementById('15').addEventListener('mouseleave', darken)
 
+// document.getElementById('rat20').addEventListener('mouseenter', ping)
 
-//Start button event listener calls setGame
-//startButton.addEventListener('click', setGame)
