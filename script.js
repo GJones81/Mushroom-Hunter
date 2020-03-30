beb11501eb54708fcf4bd45ac8c8c1c8e39560d1
gjones81 <jones.guy81@gmail.com>
2020-03-30 16:04:42 -0700

    Redoing psuedocode and planning

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+
 let startButton = document.getElementById('startButton')
 
+//first part of flashlight effect
+const lighten = (e) => {
+	e.target.style.backgroundColor = 'yellow'
+	console.log(e.target)
+}
 
-// const lighten = (e) => {
-// 	e.target.style.backgroundColor = 'yellow'
-// 	console.log(e.target)
-// }
+//second part of flashligth effect
+const darken = (e) => {
+	e.target.style.backgroundColor = 'black'
+	console.log(e.target)
+}
 
-// const darken = (e) => {
-// 	e.target.style.backgroundColor = 'black'
-// 	console.log(e.target)
-// }
-
-// const ping = (e) => {
-// 	e.target.style.backgroundColor = 'yellow'
-// 	console.log(e.target)
-// }
-
-// document.getElementById('15').addEventListener('mouseenter', lighten)
-// document.getElementById('15').addEventListener('mouseleave', darken)
-
-// document.getElementById('rat20').addEventListener('mouseenter', ping)
-
+//adds event listeners to set up 'flashlight' effect
+const startFlashlight = (e) => {
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].addEventListener('mouseenter', lighten)
+		squares[i].addEventListener('mouseleave', darken)
+	}
+}
+startButton.addEventListener('click', startFlashlight)
