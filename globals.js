60aa9db6fb801e769ef31023bff0d02ef2fb6f1d
gjones81 <jones.guy81@gmail.com>
2020-03-30 10:53:39 -0700

    updating

--- globals.js
+++ globals.js
@@ -6,19 +6,19 @@ const square3 = document.getElementById('3')
 const square4 = document.getElementById('4')
 const square5 = document.getElementById('5')
 const square6 = document.getElementById('6')
-const sqaure7 = document.getElementById('7')
+const square7 = document.getElementById('7')
 const sqaure8 = document.getElementById('8')
-const sqaure9 = document.getElementById('9')
-const sqaure10 = document.getElementById('10')
-const sqaure11 = document.getElementById('11')
-const sqaure12 = document.getElementById('12')
-const sqaure13 = document.getElementById('13')
-const sqaure14 = document.getElementById('14')
-const sqaure15 = document.getElementById('15')
-const sqaure16 = document.getElementById('16')
-const sqaure17 = document.getElementById('17')
-const sqaure18 = document.getElementById('18')
-const sqaure19 = document.getElementById('19')
+const square9 = document.getElementById('9')
+const square10 = document.getElementById('10')
+const square11 = document.getElementById('11')
+const square12 = document.getElementById('12')
+const square13 = document.getElementById('13')
+const square14 = document.getElementById('14')
+const square15 = document.getElementById('15')
+const square16 = document.getElementById('16')
+const square17 = document.getElementById('17')
+const square18 = document.getElementById('18')
+const square19 = document.getElementById('19')
 const square20 = document.getElementById('20')
 const square21 = document.getElementById('21')
 const square22 = document.getElementById('22')
@@ -26,19 +26,19 @@ const square23 = document.getElementById('23')
 const square24 = document.getElementById('24')
 const square25 = document.getElementById('25')
 const square26 = document.getElementById('26')
-const sqaure27 = document.getElementById('27')
-const sqaure28 = document.getElementById('28')
-const sqaure29 = document.getElementById('29')
-const sqaure30 = document.getElementById('30')
-const sqaure31 = document.getElementById('31')
-const sqaure32 = document.getElementById('32')
-const sqaure33 = document.getElementById('33')
-const sqaure34 = document.getElementById('34')
-const sqaure35 = document.getElementById('35')
-const sqaure36 = document.getElementById('36')
-const sqaure37 = document.getElementById('37')
-const sqaure38 = document.getElementById('38')
-const sqaure39 = document.getElementById('39')
+const square27 = document.getElementById('27')
+const square28 = document.getElementById('28')
+const square29 = document.getElementById('29')
+const square30 = document.getElementById('30')
+const square31 = document.getElementById('31')
+const square32 = document.getElementById('32')
+const square33 = document.getElementById('33')
+const square34 = document.getElementById('34')
+const square35 = document.getElementById('35')
+const square36 = document.getElementById('36')
+const square37 = document.getElementById('37')
+const square38 = document.getElementById('38')
+const square39 = document.getElementById('39')
 const square40 = document.getElementById('40')
 const square41 = document.getElementById('41')
 const square42 = document.getElementById('42')
@@ -46,19 +46,19 @@ const square43 = document.getElementById('43')
 const square44 = document.getElementById('44')
 const square45 = document.getElementById('45')
 const square46 = document.getElementById('46')
-const sqaure47 = document.getElementById('47')
-const sqaure48 = document.getElementById('48')
-const sqaure49 = document.getElementById('49')
-const sqaure50 = document.getElementById('50')
-const sqaure51 = document.getElementById('51')
-const sqaure52 = document.getElementById('52')
-const sqaure53 = document.getElementById('53')
-const sqaure54 = document.getElementById('54')
-const sqaure55 = document.getElementById('55')
-const sqaure56 = document.getElementById('56')
-const sqaure57 = document.getElementById('57')
-const sqaure58 = document.getElementById('58')
-const sqaure59 = document.getElementById('59')
+const square47 = document.getElementById('47')
+const square48 = document.getElementById('48')
+const square49 = document.getElementById('49')
+const square50 = document.getElementById('50')
+const square51 = document.getElementById('51')
+const square52 = document.getElementById('52')
+const square53 = document.getElementById('53')
+const square54 = document.getElementById('54')
+const square55 = document.getElementById('55')
+const square56 = document.getElementById('56')
+const square57 = document.getElementById('57')
+const square58 = document.getElementById('58')
+const square59 = document.getElementById('59')
 const square60 = document.getElementById('60')
 const square61 = document.getElementById('61')
 const square62 = document.getElementById('62')
@@ -66,20 +66,107 @@ const square63 = document.getElementById('63')
 const square64 = document.getElementById('64')
 const square65 = document.getElementById('65')
 const square66 = document.getElementById('66')
-const sqaure67 = document.getElementById('67')
-const sqaure68 = document.getElementById('68')
-const sqaure69 = document.getElementById('69')
-const sqaure70 = document.getElementById('70')
-const sqaure71 = document.getElementById('71')
-const sqaure72 = document.getElementById('72')
-const sqaure73 = document.getElementById('73')
-const sqaure74 = document.getElementById('74')
-const sqaure75 = document.getElementById('75')
-const sqaure76 = document.getElementById('76')
-const sqaure77 = document.getElementById('77')
-const sqaure78 = document.getElementById('78')
-const sqaure79 = document.getElementById('79')
-const sqaure80 = document.getElementById('80')
-const sqaure81 = document.getElementById('81')
-const sqaure82 = document.getElementById('82')
-const sqaure83 = document. getElementById('83')
+const square67 = document.getElementById('67')
+const square68 = document.getElementById('68')
+const square69 = document.getElementById('69')
+const square70 = document.getElementById('70')
+const square71 = document.getElementById('71')
+const square72 = document.getElementById('72')
+const square73 = document.getElementById('73')
+const square74 = document.getElementById('74')
+const square75 = document.getElementById('75')
+const square76 = document.getElementById('76')
+const square77 = document.getElementById('77')
+const square78 = document.getElementById('78')
+const square79 = document.getElementById('79')
+const square80 = document.getElementById('80')
+const square81 = document.getElementById('81')
+const square82 = document.getElementById('82')
+const square83 = document. getElementById('83')
+
+const squares = [
+square0,
+square1,
+square2,
+square3,
+square4,
+square5,
+square6,
+square7,
+sqaure8,
+square9,
+square10,
+square11,
+square12,
+square13,
+square14,
+square15,
+square16,
+square17,
+square18,
+square19,
+square20,
+square21,
+square22,
+square23,
+square24,
+square25,
+square26,
+square27,
+square28,
+square29,
+square30,
+square31,
+square32,
+square33,
+square34,
+square35,
+square36,
+square37,
+square38,
+square39,
+square40,
+square41,
+square42,
+square43,
+square44,
+square45,
+square46,
+square47,
+square48,
+square49,
+square50,
+square51,
+square52,
+square53,
+square54,
+square55,
+square56,
+square57,
+square58,
+square59,
+square60,
+square61,
+square62,
+square63,
+square64,
+square65,
+square66,
+square67,
+square68,
+square69,
+square70,
+square71,
+square72,
+square73,
+square74,
+square75,
+square76,
+square77,
+square78,
+square79,
+square80,
+square81,
+square82,
+square83,
+]
