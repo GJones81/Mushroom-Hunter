95a7344abfb323fa413ab99390c8af6265093c34
gjones81 <jones.guy81@gmail.com>
2020-03-30 18:58:34 -0700

    updating

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-
+//Globals
+let turnNumber = 0
 let mushroomNumber = 0
-let ratNumber = 0
+let ratHorde = []
+let score = 0
+let healthPoints = 3
+
 let startButton = document.getElementById('startButton')
 let titleArea = document.getElementById('titleArea')
 
@@ -16,13 +20,48 @@ const darken = (e) => {
 	console.log(e.target)
 }
 
-//randomizer function 
+// function resets game
+	//sets score to 0
+	//sets health points to 3
+	//clears ratHorde
+	//remove event listeners
+
+
+//function for ending game
+	//append score to high score table
+	//display high score table
+	//ask if player wants to play again
+	// if so, calls resetGame
+
+//function for clicking mushroom
+	//increase score by one
+	//calls setLevel function for next level
+
+//function for mousing over rat
+	//subtract 1 from healthPoints
+	//check if healthPoints are at 0
+	//	calls endGame function if at 0
+
 
 //function to set mushroom
-	//calls randomizer function
+	//randomizer
+	//	sets mushroomNumber to number
+	//adds event listener to mushroom image
+	//remove event listeners for 'flashlight'
+	//adds mushroom image based on mushroomNumber
 
-//function to set rat, or rats
-	//calls randomizer function
+//function to create rat, or rats
+	//randomizer
+	//checks if random number matches mushroomNumber or
+	//	other numbers already in the ratHorde array
+	//push new number into ratHorde
+	
+//function to set rats
+	//based on numbers in ratHorde and .length
+		//add event listener to rat images
+		//remove event listeners for 'flashlight'
+		//add rat images based on random numbers popped off
+		//	the array
 
 //adds event listeners to set up 'flashlight' effect
 const setLevel = (e) => {
@@ -30,6 +69,7 @@ const setLevel = (e) => {
 		squares[i].addEventListener('mouseenter', lighten)
 		squares[i].addEventListener('mouseleave', darken)
 	}
+	turnNumber++
 	titleArea.style.display = 'none'
 	//call function to place mushroom
 
