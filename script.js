bed4251fd6b48ec2aadd3b6930c271b6c7156b65
gjones81 <jones.guy81@gmail.com>
2020-03-30 17:54:23 -0700

    updating

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-let ratNumber = -1
+let mushroomNumber = 0
+let ratNumber = 0
 let startButton = document.getElementById('startButton')
 let titleArea = document.getElementById('titleArea')
 
@@ -15,12 +16,24 @@ const darken = (e) => {
 	console.log(e.target)
 }
 
+//randomizer function 
+
+//function to set mushroom
+	//calls randomizer function
+
+//function to set rat, or rats
+	//calls randomizer function
+
 //adds event listeners to set up 'flashlight' effect
-const startGame = (e) => {
+const setLevel = (e) => {
 	for (let i = 0; i < squares.length; i++) {
 		squares[i].addEventListener('mouseenter', lighten)
 		squares[i].addEventListener('mouseleave', darken)
 	}
 	titleArea.style.display = 'none'
+	//call function to place mushroom
+
+	//call function to place rat, or rats
 }
-startButton.addEventListener('click', startGame)
+
+//startButton.addEventListener('click', setLevel)
