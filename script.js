3f2ffacebcd23702e858da6ac6fd16d3a84ad2f3
gjones81 <jones.guy81@gmail.com>
2020-04-01 15:32:11 -0700

    Functionality pretty much done. Trying to refine the styling and layout. This commit has elements 'turned off' for the purpose of styling endgame elements

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Globals
+const LS_NAME = 'highScores'
+const SCORE_SLOTS = 5
 let turnNumber = 0
 let mushroomNumber = 0
 let ratHorde = []
@@ -13,91 +15,160 @@ let defeatBanner = document.getElementById('defeatBanner')
 
 let playAgainButton = document.getElementById('playAgain')
 
+let highScoreTable = document.getElementById('highScoreTable')
+
+let table = document.getElementById('table')
+
 let statusArea = document.getElementById('statusArea')
 
 let healthBoard = document.getElementById('health')
 
 let scoreBoard = document.getElementById('score')
 
+let continueButtonArea = document.getElementById('contButtonArea')
+
 let continueButton = document.getElementById('continueButton')
+
+let exitButtonArea = document.getElementById('exitButtonArea')
 
 let exitButton = document.getElementById('exitButton')
 
 let playArea = document.getElementById('playArea')
 
+let transitionArea = document.getElementById('transitionArea')
 
-//first part of flashlight effect
-const lighten = (e) => {
-	e.target.style.backgroundColor = 'yellow'
+
+document.addEventListener('DOMContentLoaded', () => {
+	//sets a click on the start button
+	startButton.addEventListener('click', setLevel)
+})
+
+const resetGame = () => {
+	score = 0
+	healthPoints = 10
+	turnNumber = 0 
+	defeatBanner.style.display = 'none'
+	endLevel()
 }
 
-//second part of flashligth effect
-const darken = (e) => {
-	e.target.style.backgroundColor = 'black'
-	e.target.style.backgroundImage = 'none'
+const retrieveHighScore = () => {
+	//goes into local storage and retrieves scores in string type format
+	let scoreString = localStorage.getItem(LS_NAME)
+		//if there aren't any strings in local storage, return an empty array
+		if (!scoreString){
+			return []
+ 		}
+ 		//convert the strings to an array and return it when called
+ 		let scoreArray = JSON.parse(scoreString)
+ 		return scoreArray	
 }
 
-const getRandomInt = (max) => {
-	return Math.floor(Math.random() * Math.floor(max))
+const tallyHighScore = (initials) => {
+	//calls the function to get the array it returns from local storage
+	let scores = retrieveHighScore()
+	//add the new score to the array
+	scores.push({initials: initials, score: score})
+	//sort the array to put them in order from highest to lowest
+	scores.sort((a, b) => {
+		return b.score - a.score
+	})
+	//limit the score table entries to the top 10
+	scores = scores.slice(0, SCORE_SLOTS)
+	//stringify the updated array
+	let newScoreString = JSON.stringify(scores)
+	//send it back to local storage
+	localStorage.setItem(LS_NAME, newScoreString)	
 }
 
-// function resets game
-	//sets score to 0
-	//sets health points to 3
-	//clears ratHorde
-	//remove event listeners
+const updateScore = () => {
+	//clear the table
+	document.getElementById('tableBody').innerHTML = ''
+	//get scores from local storage
+	let scores = retrieveHighScore()
+	//loop through the scores
+	for ( let i = 0; i < scores.length; i++){
+		let tr = document.createElement('tr')
+		let tdInitials = document.createElement('td')
+		let tdScore = document.createElement('td')
+		tdInitials.textContent = scores[i].initials
+		tdScore.textContent = scores[i].score
+		tr.append(tdInitials)
+		tr.append(tdScore)
+		document.getElementById('tableBody').append(tr)
+	}
+}
 
 
 //function for ending game
 const endGame = () => {
+	//hides the playing area
 	playArea.style.display = 'none'
+	//hides the status area
 	statusArea.style.display = 'none'
+	//shows the defeat announcement and highscore table
 	defeatBanner.style.display = 'inline-block'
-}
-	//append score to high score table
-	//display high score table
+	//ask player to record initials
+	let initials = prompt("Nice huntin'. Write your initials here")
+	//add up the score
+	tallyHighScore(initials)
+	//update high score
+	updateScore()
+		
 	//ask if player wants to play again
 	// if so, calls resetGame
-
-//function for clicking on a mushroom
-const claimMushroom = () =>{
-	playArea.style.display = 'none'
-	score++
-	scoreBoard.textContent = `Your score is ${score}`
-	healthBoard.textContent = `Your health is at ${healthPoints}`
-	continueButton.addEventListener('click', endLevel)
 }
+
 //function for mousing over rat
 const ratAttack = (e) => {
 	console.log('ATTACK!')
+	//set the image of the rat
 	e.target.style.backgroundImage = "url('pngkey.com-animal-face-png-3882222.png')"
+	//subtracts health points and updates the health points displayed
 	healthPoints--
+	healthBoard.textContent = `Your health is at ${healthPoints}`
 	console.log(healthPoints)
+	//calls endGame function when health points fall to zero
 	if (healthPoints == 0){
 		endGame()
 	}
 }
 
-//function to set background image to mushroom
-const findMushroom =(e) => {
-	squares[mushroomNumber].style.backgroundImage = "url('mushroom-icon-2-48.png')"
+//function for clicking on a mushroom
+const claimMushroom = () =>{
+	//increases score
+	score++
+	//hides the playing area
+	playArea.style.display = 'none'
+	//shows the continue button
+	contButtonArea.style.display = 'inline-block'
+	//updates the score displayed
+	scoreBoard.textContent = `Your score is ${score}`
+	//add a click to the endLevel function
+	continueButton.addEventListener('click', endLevel)
 }
 
+//function to set background image to mushroom
+const findMushroom =(e) => {
+	//set the image of a mushroom
+	squares[mushroomNumber].style.backgroundImage = "url('mushroom-icon-2-48.png')"
+	//play a short audio clip?
+}
+
+//randomizer function
+const getRandomInt = (max) => {
+	return Math.floor(Math.random() * Math.floor(max))
+}
 
 //function to create a random number and set the mushroom with that number
 const setMushroom = (e) => {
-	//mushroomNumber = Math.floor(Math.random() * 100)
+	//calls getRandomInt to get a random number, then assigns it to mushroomNumber
 	mushroomNumber = getRandomInt(84)
-	console.log('the mushroom number equals ' + mushroomNumber)
-	if (mushroomNumber < 84){
+	console.log('the mushroom is at ' + mushroomNumber)
 		//add event listeners
 		squares[mushroomNumber].addEventListener('mouseenter', findMushroom)
 		squares[mushroomNumber].addEventListener('click', claimMushroom)
-	} 
-	//else {
-	//	setMushroom()
-	//}
-//calls function to begin populating the rat horde
+
+	//calls function to begin populating the rat horde
 	countRatHorde()
 }
 	
@@ -106,9 +177,9 @@ const countRatHorde = () => {
 	if (turnNumber > 1){
 		while (ratHorde.length < turnNumber - 1 ){
 			let pup = getRandomInt(84)
-			//let pup = Math.floor(Math.random() * 100)
-			if (pup < 84 && ratHorde.includes(pup) != true && pup != mushroomNumber){
+			if (ratHorde.includes(pup) != true && pup != mushroomNumber){
 				ratHorde.push(pup)
+				console.log(ratHorde)
 			}
 		}
 		createRats()
@@ -119,11 +190,24 @@ const countRatHorde = () => {
 const createRats = () => {
 		while(ratHorde.length > 0){
 			rat = ratHorde.pop()
-			console.log('rat equals ' + rat)
+			console.log('rat at ' + rat)
 			squares[rat].removeEventListener('mouseenter', lighten)
-			//ratAttack is the function which shows the rat image and decrements healthPoints
+			//ratAttack is the function which shows the rat image and 
+			//decrements healthPoints
 			squares[rat].addEventListener('mouseenter', ratAttack)
+			console.log(ratHorde)
 		}
+}
+
+//first part of flashlight effect
+const lighten = (e) => {
+	e.target.style.backgroundColor = '#ffff33'
+}
+
+//second part of flashligth effect
+const darken = (e) => {
+	e.target.style.backgroundColor = 'black'
+	e.target.style.backgroundImage = 'none'
 }
 
 //Adds event listeners to set up 'flashlight' effect
@@ -132,10 +216,19 @@ const setLevel = (e) => {
 		squares[i].addEventListener('mouseenter', lighten)
 		squares[i].addEventListener('mouseleave', darken)
 	}
+	//increases turn number
 	turnNumber++
+	healthBoard.textContent = `Your health is at ${healthPoints}`
+	scoreBoard.textContent = `Your score is ${score}`
 	console.log('It is turn number ' + turnNumber)
+	//hides the title area for playing
 	titleArea.style.display = 'none'
+	//shows the status area for playing
 	statusArea.style.display = 'inline-block'
+	//hides the continue button area
+	contButtonArea.style.display = 'none'
+	//shows the exit button
+	exitButtonArea.style.display = 'inline-block'
 	//call function to place mushroom
 	setMushroom()
 
@@ -143,21 +236,30 @@ const setLevel = (e) => {
 
 //removes all event listeners to conclude the level before starting the next
 const endLevel = () => {
+	//shows the playing area
 	playArea.style.display = 'inline-block'
+	//removes the click from the continue button
 	continueButton.removeEventListener('click', endLevel)
-	console.log('wrapping up the level')
+	//resets the squares with mushrooms to 'blank' for the next level to begin 
 	squares[mushroomNumber].style.backgroundImage = 'none'
 	squares[mushroomNumber].style.backgroundColor = 'black'
+	//cycles through to clear all event listeners and rat images for the
+	//next level to begin
 	for (let i = 0; i < squares.length; i++) {
 		squares[i].removeEventListener('mouseenter', lighten)
 		squares[i].removeEventListener('mouseleave', darken)
 		squares[i].removeEventListener('mouseenter', findMushroom)
 		squares[i].removeEventListener('click', claimMushroom)
+		squares[i].removeEventListener('mouseenter', ratAttack)
 		squares[i].style.backgroundImage = 'none'
 	}
+	//resets these global variables for the next level to begin
 	mushroomNumber = 0
 	ratHorde = []
 	setLevel()
 }
 
-startButton.addEventListener('click', setLevel)
+//sets a click on the reset button
+playAgainButton.addEventListener('click', resetGame)
+//sets a click on the exit button
+exitButton.addEventListener('click', endGame)
